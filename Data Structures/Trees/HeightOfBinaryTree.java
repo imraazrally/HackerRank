8df228bded8fbf8cfc9e36d1f89dce1375e4af8e
Imraaz Rally <raisoft2020@gmail.com>
2015-11-08 08:09:34 -0500

    Update HeightOfBinaryTree.java

--- Data Structures/Trees/HeightOfBinaryTree.java
+++ Data Structures/Trees/HeightOfBinaryTree.java
@@ -1,6 +1,6 @@
 int height(Node root){
         //base
         if(root==null) return 0;
-        //Getting the maximum length from the left sub-tree.
+        //Getting the maximum length of the left sub-tree vs right sub-tree.
         return Math.max(height(root.left)+1, height(root.right)+1);
 }
